1a7183ab839b625c8e360b750981e1c01de37c65
Rafael Neves <106340904+rafaelwarren@users.noreply.github.com>
2022-10-19 11:03:01 -0300

    adicionado configuracao de acesso a banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { StockService } from './services/stock.service';
 import { FundService } from './services/fund.service';
 import { CashService } from './services/cash.service';
 import { PortfolioService } from './services/portfolio.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -17,6 +18,16 @@ import { PortfolioService } from './services/portfolio.service';
         username: process.env.BRITECH_USERNAME,
         password: process.env.BRITECH_PASSWORD,
       },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'sql-meuportfolio.cvy5lr8wtvss.sa-east-1.rds.amazonaws.com',
+      port: 1433,
+      username: 'usr_integrationVitra',
+      password: 'x9Rr6bFDHbp%2L3',
+      database: 'integrationVitra',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
     }),
   ],
   providers: [
